06df0c85963b397ed7cd4ac7cbb264f66f897cee
camayeyo <60127988+camayeyo@users.noreply.github.com>
2020-08-25 23:28:42 +0500

    remake commit

---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+$to  = 'azizdusmuhamedov@gmail.com' . ', ';
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("azizdusmuhamedov@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
