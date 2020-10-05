e53e0fc2e4fc1be2542f5ac378e901d36843b616
camayeyo <60127988+camayeyo@users.noreply.github.com>
2020-10-05 20:18:45 +0500

    contact fix 1.7

--- js/scripts.js
+++ js/scripts.js
@@ -23,22 +23,6 @@ $(document).ready(function(){
     type: 'image',
     gallery:{enabled:true}
   });
-
-  $("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-  });
   
 });
 
--- mail.php
+++ mail.php
@@ -1,40 +1,45 @@
-<?php 
-
-require_once('phpmailer/PHPMailerAutoload.php');
+<?php
+ 
+require 'PHPMailer/PHPMailerAutoload.php';
+ 
 $mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
 
 $name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'websitemailbox@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Website__mailb0X'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('websitemailbox@yandex.ru'); // от кого будет уходить письмо?
-$mail->addAddress('azizdusmuhamedov@gmail.com');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
-
+$subject = $_POST['form_subject'];
+ 
+//Дебаг
+//0 = off (for production use)
+//1 = клиентские сообщения
+//2 = серверные и клиентские сообщения
+//$mail->SMTPDebug = 2;
+ 
+$mail->isSMTP();
+$mail->Host = 'smtp.yandex.ru'; //gmail: smtp.gmail.com
+$mail->SMTPAuth = true;
+$mail->Username = 'websitemailbox@yandex.ru';
+$mail->Password = 'Website__mailb0X';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+$mail->setLanguage('ru');
+$mail->setFrom('websitemailbox@yandex.ru', 'Robot');
+$mail->addAddress('azizdusmuhamedov@gmail.com', 'My Friend');    //Получатель
+//$mail->addAddress('my.friend2@gmail.com');              //Еще получатель
+//$mail->addReplyTo('my.friend3@gmail.com', 'My Friend 3');
+//$mail->addCC('my.friend.cc@example.com');               //Копия
+//$mail->addBCC('my.friend.bcc@example.com');             //Скрытая копия
+ 
+//$mail->addAttachment('/path/to/file.zip');              //Прикрепить файл
+//$mail->addAttachment('/path/to/image.jpg', 'new.jpg');  //Прикрепить файл + задать имя
+$mail->isHTML(true);
+ 
+$mail->Subject = 'Тема письма';
+$mail->Body    = '<b>HTML</b> версия письма';
+$mail->AltBody = 'Текстовая версия письма, без HTML тегов (для клиентов не поддерживающих HTML)';
+ 
+//Отправка сообщения
 if(!$mail->send()) {
-    echo 'Error';
+    echo 'Ошибка при отправке. Ошибка: ' . $mail->ErrorInfo;
 } else {
-    header('location: thank-you.html');
+    echo 'Сообщение успешно отправлено';
 }
-?>
